f3e6e6c6d78bb21a038cac79c076c0a140c67efb
David Thomas <david.thomas214@yahoo.com>
2014-10-13 13:29:19 -0700

    improve Assignement2

--- Assignment2/Assignment2.Exercise1.scala
+++ Assignment2/Assignment2.Exercise1.scala
@@ -1,27 +1,22 @@
-type Set = Int => Boolean
-
+type Set = Int => Boolean;
 def singletonSet(elem: Int): Set = Set(elem);
-
 def contains(s: Set, elem: Int): Boolean = s(elem);
-def union(s: Set, t: Set): Set = s.union(t);
+def union(s: Set, t: Set): Set = Set(x,y);
 def intersect(s: Set, t: Set): Set = s.intersect(t);
 def diff(s: Set, t: Set): Set = s.diff(t);
-def filter(s: Set, p: Int => Boolean): Set = s.filter(p)
-
+def filter(s: Set, p: Int => Boolean): Set = s.filter(p);
+def exists(s: Set, p: Int => Boolean): Boolean = s.exists(p);
+def map(s: Set, f: Int => Int): Set = s.map(f);
 def forall(s: Set, p: Int => Boolean): Boolean = {
 	def iter(a: Int): Boolean = {
-		if() {
-		
+		if(a < value) {
+		  return false;
 		} else if() {
-		
+		  
 		} else {
-			iter()
+			iter(a+1);
 		}
 	}
 
-	iter(p);
+	iter();
 }
-
-def exists(s: Set, p: Int => Boolean): Boolean
-
-def map(s: Set, f: Int => Int): Set
