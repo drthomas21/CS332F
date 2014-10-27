ce691e9cc3bb2b3648b81332ca60c0cf46adc700
David Thomas <david.thomas214@yahoo.com>
2014-10-27 16:18:11 -0400

    Adding assignment 3

--- Assignment2/Assignment2.Exercise1.scala
+++ Assignment2/Assignment2.Exercise1.scala
@@ -1,22 +1,44 @@
 type Set = Int => Boolean;
 def singletonSet(elem: Int): Set = Set(elem);
+
 def contains(s: Set, elem: Int): Boolean = s(elem);
-def union(s: Set, t: Set): Set = Set(x,y);
-def intersect(s: Set, t: Set): Set = s.intersect(t);
-def diff(s: Set, t: Set): Set = s.diff(t);
-def filter(s: Set, p: Int => Boolean): Set = s.filter(p);
-def exists(s: Set, p: Int => Boolean): Boolean = s.exists(p);
-def map(s: Set, f: Int => Int): Set = s.map(f);
+
+def union(s: Set, t: Set): Set = i => s(i) || t(i);
+def intersect(s: Set, t: Set): Set = i => s(i) && t(i);
+def diff(s: Set, t: Set): Set = i => s(i) && !t(i);
+
+def filter(s: Set, p: Int => Boolean): Set = {
+  var x: Set = p;
+  return intersect(s,x);
+}
+
 def forall(s: Set, p: Int => Boolean): Boolean = {
 	def iter(a: Int): Boolean = {
-		if(a < value) {
-		  return false;
-		} else if() {
-		  
+		if(s(a) && p(a)) {
+			return true;
+		} else if((s(a) && !p(a)) || a >= 1000) {
+			return false;
 		} else {
-			iter(a+1);
+			return iter(a+1);
 		}
 	}
 
-	iter();
+	return iter(-1000);
 }
+
+def exists(s: Set, p: Int => Boolean): Boolean = {
+	def iter(a: Int): Boolean = {
+		if(s(a) && p(a)) {
+			return true;
+		} else if( a >= 1000) {
+			return false;
+		} else {
+			return iter(a+1);
+		}
+	}
+
+	return iter(-1000);
+}
+
+def map(s: Set, f: Int => Int): Set = Set(f,s);
+
---
+++ Assignment3/Assignment3.Exercise1.scala
@@ -0,0 +1,48 @@
+class Rational(inclNum: Int = 1,inclDen: Int = 1) {
+	var num: Int = inclNum;
+	var den: Int = inclDen;
+
+	var defNum: Int = 1;
+	var defDen: Int = 1;
+
+	var inc: Int = 1;
+
+	def setNumerator(i: Int) = num = i;
+	def setDenominator(i: Int) = den = i;
+	def getNumerator(): Int = num;
+	def getDenominator(): Int = den;
+	def incNumerator() = num += inc;
+	def incDenominator() = den += inc;
+
+	def getNumber(): Double = {
+		var dNum: Double = num;
+		var dDen: Double = den;
+		return dNum/dDen
+	};
+	def / (r: Rational): Rational = {
+		var ret: Rational = new Rational();
+		ret.setNumerator(num * r.getDenominator());
+		ret.setDenominator(den * r.getNumerator());
+		return ret;
+	};
+	def sum (f: Rational => Rational)(b: Rational): Rational = {
+		var a: Rational = new Rational(1,1);
+		return sumf(f)(a,b);
+	};
+	def sumf(f: Rational => Rational)(a: Rational,b: Rational):Rational = {
+		if(a.getNumerator() == a.getDenominator()) {
+			a.incDenominator();
+			a.setNumerator(1);
+			return sumf(f)(a,b);
+		} else if(a.getNumber() <= b.getNumber()) {
+			var fraction: Rational = new Rational(a.getNumerator(),b.getNumerator());
+			a.incNumerator();
+			var c: Rational = f(fraction);
+			var d: Rational = sumf(f)(a,b);
+
+			return new Rational((c.getNumerator() * d.getDenominator() + d.getNumerator() * c.getDenominator()),(c.getDenominator() * d.getDenominator()));
+		} else {
+			return new Rational(0,1);
+		}
+	};
+}
